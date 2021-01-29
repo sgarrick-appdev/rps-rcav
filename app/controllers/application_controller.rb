d98ded6721630bbf809ad5fb56ab35c74880119a
sgarrick <sgarrick@uchicago.edu>
2021-01-29 21:08:37 +0000

    progress on paper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,28 @@ class ApplicationController < ActionController::Base
     render({:template => "game_templates/user_rock.html.erb"})
   
   end
+  
+def rules
+    #write a ton of code
+    #redirect_to("https://wikipedia.com")
+    #render({:html => "<h1>Hello, world!</h1>".html_safe})
+    
+    render({:template => "game_templates/rules.html.erb"})
+  end
+
+  def play_paper
+    
+   @comp_move= ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+
+    end
+  
+    render({:template => "game_templates/paper.html.erb"})
+  end
+
 end
