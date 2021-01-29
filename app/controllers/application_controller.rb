c897be4dc11e0ea501f652aaaf8aac86b35a7507
sgarrick <sgarrick@uchicago.edu>
2021-01-29 20:45:40 +0000

    /rock render works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,12 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
-
+  def play_rock
+    #write a ton of code
+    #redirect_to("https://wikipedia.com")
+    #render({:html => "<h1>Hello, world!</h1>".html_safe})
+    
+    render({:template => "game_templates/user_rock.html.erb"})
+  
+  end
 end
